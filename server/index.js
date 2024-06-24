835bd4ad8975730575711aacc8baa9802762dc15
W E D Y <110571114+WedyDev@users.noreply.github.com>
2024-06-24 16:40:38 -0500

    Vercel Deploy

--- server/index.js
+++ server/index.js
@@ -7,40 +7,45 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: "34.45.57.129",
-    user: "testfractal",
-    password: "walter123",
-    database: "orders"
+    host: process.env.DB_HOST || "34.45.57.129",
+    user: process.env.DB_USER || "testfractal",
+    password: process.env.DB_PASSWORD || "walter123",
+    database: process.env.DB_NAME || "orders"
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error("Database connection failed: " + err.stack);
+        return;
+    }
+    console.log("Connected to database.");
 });
 
 app.post("/create", (req, res) => {
-    const Order = req.body.Order;
-    const Date = req.body.Date;
-    const Products = req.body.Products;
-    const FinalPrice = req.body.FinalPrice;
-    const Status = req.body.Status;
+    const { Order, Date, Products, FinalPrice, Status } = req.body;
 
-    db.query('INSERT INTO orders (`Order`, `Date`, `Products`, `FinalPrice`, `Status`) values(?,?,?,?,?)', [Order, Date, Products, FinalPrice, Status],
+    db.query('INSERT INTO orders (`Order`, `Date`, `Products`, `FinalPrice`, `Status`) values(?,?,?,?,?)', 
+             [Order, Date, Products, FinalPrice, Status],
         (err, result) => {
             if (err) {
-                console.log(err)
+                console.log(err);
+                res.status(500).send(err);
             } else {
                 res.send(result);
             }
         }
-    )
+    );
 });
 
 app.get("/orders", (req, res) => {
-    db.query('SELECT * FROM orders',
-        (err, result) => {
-            if (err) {
-                console.log(err)
-            } else {
-                res.send(result);
-            }
+    db.query('SELECT * FROM orders', (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);
+        } else {
+            res.send(result);
         }
-    )
+    });
 });
 
 app.get("/orders/:id", (req, res) => {
@@ -48,6 +53,7 @@ app.get("/orders/:id", (req, res) => {
     db.query('SELECT * FROM orders WHERE `id` = ?', [id], (err, result) => {
         if (err) {
             console.log(err);
+            res.status(500).send(err);
         } else {
             if (result.length > 0) {
                 res.send(result[0]);
@@ -68,6 +74,7 @@ app.put("/update/:id", (req, res) => {
         (err, result) => {
             if (err) {
                 console.log(err);
+                res.status(500).send(err);
             } else {
                 res.send(result);
             }
@@ -78,15 +85,14 @@ app.put("/update/:id", (req, res) => {
 app.delete("/delete/:id", (req, res) => {
     const id = req.params.id;
 
-    db.query('DELETE FROM orders WHERE `id`=?', id,
-        (err, result) => {
-            if (err) {
-                console.log(err)
-            } else {
-                res.send(result);
-            }
+    db.query('DELETE FROM orders WHERE `id`=?', [id], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);
+        } else {
+            res.send(result);
         }
-    )
+    });
 });
 
 const port = process.env.PORT || 3001;
