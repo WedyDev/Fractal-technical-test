7c61a338f4ba4311c39cb2a826a4ebf518327114
W E D Y <110571114+WedyDev@users.noreply.github.com>
2024-06-24 16:24:47 -0500

    reanme versel

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const app=express();
-const mysql=require("mysql");
+const app = express();
+const mysql = require("mysql");
 const cors = require("cors");
 
 app.use(cors());
@@ -13,94 +13,83 @@ const db = mysql.createConnection({
     database: "orders"
 });
 
-app.post("/create", (req,res)=>{
-    const Order=req.body.Order;
-    const Date=req.body.Date;
-    const Products=req.body.Products;
-    const FinalPrice=req.body.FinalPrice;
-    const Status=req.body.Status;
+app.post("/create", (req, res) => {
+    const Order = req.body.Order;
+    const Date = req.body.Date;
+    const Products = req.body.Products;
+    const FinalPrice = req.body.FinalPrice;
+    const Status = req.body.Status;
 
-    db.query('INSERT INTO orders (`Order`, `Date`, `Products`, `FinalPrice`, `Status`) values(?,?,?,?,?)',[Order,Date,Products,FinalPrice,Status],
-        (err,result)=>{
-            if(err){
+    db.query('INSERT INTO orders (`Order`, `Date`, `Products`, `FinalPrice`, `Status`) values(?,?,?,?,?)', [Order, Date, Products, FinalPrice, Status],
+        (err, result) => {
+            if (err) {
                 console.log(err)
-            }
-            else{
+            } else {
                 res.send(result);
             }
         }
     )
-
 });
 
-
-app.get("/orders", (req,res)=>{
-    
-
-    db.query('select * from orders ',
-        (err,result)=>{
-            if(err){
+app.get("/orders", (req, res) => {
+    db.query('SELECT * FROM orders',
+        (err, result) => {
+            if (err) {
                 console.log(err)
-            }
-            else{
+            } else {
                 res.send(result);
             }
         }
     )
-
 });
 
 app.get("/orders/:id", (req, res) => {
     const id = req.params.id;
     db.query('SELECT * FROM orders WHERE `id` = ?', [id], (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        if (result.length > 0) {
-          res.send(result[0]);
+        if (err) {
+            console.log(err);
         } else {
-          res.status(404).send({ message: "Order not found" });
+            if (result.length > 0) {
+                res.send(result[0]);
+            } else {
+                res.status(404).send({ message: "Order not found" });
+            }
         }
-      }
     });
-  });
+});
 
 app.put("/update/:id", (req, res) => {
     const id = req.params.id;
     const { Order, Date, Products, FinalPrice, Status } = req.body;
-  
+
     db.query(
-      'UPDATE orders SET `Order`=?, `Date`=?, `Products`=?, `FinalPrice`=?, `Status`=? WHERE `id`=?',
-      [Order, Date, Products, FinalPrice, Status, id],
-      (err, result) => {
-        if (err) {
-          console.log(err);
-        } else {
-          res.send(result);
-        }
-      }
-    );
-  });
-
-app.delete("/delete/:id", (req,res)=>{
-
-    const id=req.params.id;
-  
-
-    db.query('DELETE FROM orders  WHERE `id`=?',id,
-        (err,result)=>{
-            if(err){
-                console.log(err)
+        'UPDATE orders SET `Order`=?, `Date`=?, `Products`=?, `FinalPrice`=?, `Status`=? WHERE `id`=?',
+        [Order, Date, Products, FinalPrice, Status, id],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
             }
-            else{
+        }
+    );
+});
+
+app.delete("/delete/:id", (req, res) => {
+    const id = req.params.id;
+
+    db.query('DELETE FROM orders WHERE `id`=?', id,
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
                 res.send(result);
             }
         }
     )
-
 });
 
-
-app.listen(3001,()=>{
-    console.log("Connect Port 3001")
-})
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
